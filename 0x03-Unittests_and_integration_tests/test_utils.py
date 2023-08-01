7e18f763db4d19ea0028e9f6f96e8dfaf8bcc939
= <=>
2023-08-01 07:44:52 +0300

    add tasks

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,43 @@
+#!usr/bin/python3
+"""
+TestCase for TestCase
+"""
+import unittest
+
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    def test_access_nested_map(self):
+        """
+        Test of Accessing a NestedMap
+        """
+
+        # Test with a nested map
+        nested_map = {"a": {"b": {"c": 1}}}
+        self.assertEqual(access_nested_map(nested_map, ["a", "b", "c"]), 1)
+
+        # Test with a deeper nested map
+        nested_map = {"x": {"y": {"z": {"value": 42}}}}
+        self.assertEqual(
+            access_nested_map(nested_map, ["x", "y", "z", "value"]), 42
+        )
+
+    def test_invalid_key(self):
+        """
+        Test for invalid key
+        """
+
+        # Test with an invalid key
+        nested_map = {"a": {"b": {"c": 1}}}
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, ["a", "x", "y"])
+
+        # Test with an invalid path
+        nested_map = {"a": {"b": {"c": 1}}}
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, ["a", "b", "c", "d"])
+
+
+if __name__ == "__main__":
+    unittest.main()
