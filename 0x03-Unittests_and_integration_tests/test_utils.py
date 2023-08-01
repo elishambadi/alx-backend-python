4a885b7dbec906b7c86c15ae2cb6b66494d7fdcf
= <=>
2023-08-01 08:01:38 +0300

    add test utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,42 +1,25 @@
 #!usr/bin/env python3
 """
-TestCase for TestCase
+TestCase for Utils
 """
 import unittest
+from parameterized import parameterized
 
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    def test_access_nested_map(self):
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
         """
-        Test of Accessing a NestedMap
+        Test for access_nested_map
         """
-
-        # Test with a nested map
-        nested_map = {"a": {"b": {"c": 1}}}
-        self.assertEqual(access_nested_map(nested_map, ["a", "b", "c"]), 1)
-
-        # Test with a deeper nested map
-        nested_map = {"x": {"y": {"z": {"value": 42}}}}
-        self.assertEqual(
-            access_nested_map(nested_map, ["x", "y", "z", "value"]), 42
-        )
-
-    def test_invalid_key(self):
-        """
-        Test for invalid key
-        """
-
-        # Test with an invalid key
-        nested_map = {"a": {"b": {"c": 1}}}
-        with self.assertRaises(KeyError):
-            access_nested_map(nested_map, ["a", "x", "y"])
-
-        # Test with an invalid path
-        nested_map = {"a": {"b": {"c": 1}}}
-        with self.assertRaises(KeyError):
-            access_nested_map(nested_map, ["a", "b", "c", "d"])
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
 
 if __name__ == "__main__":
