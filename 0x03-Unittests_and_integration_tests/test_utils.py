6a8beedc4f718d727ed41b711c9483c9eb4f3dbb
elishambadi <embadi43@gmail.com>
2023-10-30 18:01:05 +0300

    annotation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
+from typing import Any, Dict, Tuple, Union
 
 from utils import access_nested_map, get_json
 
@@ -18,40 +19,14 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    def test_access_nested_map(self,
+                               nested_map: Dict[str, Any],
+                               path: Tuple[str, ...],
+                               result: Union[int, Dict[str, Any]]) -> None:
         """
         Test for access_nested_map
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
-
-
-class TestGetJson(unittest.TestCase):
-    """
-        Class for Testing getting json response from url
-        Extends: unittest.TestCase
-    """
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    @patch("requests.get")
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """
-            Sets up a mock response and tests getting json response
-        """
-        # Set up the mock response
-        mock_response = unittest.mock.Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
-        # Call the function under test
-        result = get_json(test_url)
-
-        # Assert mock method was called once
-        mock_get.assert_called_once_with(test_url)
-
-        # Assert the output of get_json is equal to test_payload
-        self.assertEqual(result, test_payload)
+        self.assertEqual(access_nested_map(nested_map, path), result)
 
 
 if __name__ == "__main__":
